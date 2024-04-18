0824e7e340ffde7cb1b73b4e8acc720e18bf89d2
Errickjuo22 <erickjuarez811@gmail.com>
2024-04-17 20:26:24 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes ------
+| API Routes ________--------
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
